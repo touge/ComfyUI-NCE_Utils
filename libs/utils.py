bb5aa54f4580cf585cbfc7f0798ebb4d5661801d
touge <lon.y@qq.com>
2025-02-27 14:46:01 +0800

    update

--- libs/utils.py
+++ libs/utils.py
@@ -23,10 +23,7 @@ def log(message:str, message_type:str='info'):
 try:
     from cv2.ximgproc import guidedFilter
 except ImportError as e:
-    # print(e)
-    log(f"Cannot import name 'guidedFilter' from 'cv2.ximgproc'"
-        f"\nA few nodes cannot works properly, while most nodes are not affected. Please REINSTALL package 'opencv-contrib-python'."
-        f"\nFor detail refer to \033[4mhttps://github.com/chflame163/ComfyUI_LayerStyle/issues/5\033[0m")
+    print(e)
     
 def generate_random_name(prefix: str, suffix: str, length: int) -> str:
     # 生成一个指定长度的随机字符串，并在其前后加上前缀和后缀
